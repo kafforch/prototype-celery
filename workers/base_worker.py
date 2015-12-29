675eef0dc0dcd0d322551870ee38608ad241460d
kafforch <kafforch@github.com>
2015-12-30 10:03:51 +1100

    Tucking the worker boilerplate into base_worker

---
+++ workers/base_worker.py
@@ -0,0 +1,42 @@
+from celery import Celery
+from model.plan_repo import PlanRepo
+from model.task_repo import TaskRepo
+from redis import StrictRedis
+from workers.config import WorkerConfigurator
+from celery.signals import celeryd_after_setup
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+config = WorkerConfigurator("cfg/kafforch.cfg")
+
+redis_config = config.get_redis_config_kwargs()
+redis_inst = StrictRedis(**redis_config)
+
+celery_config = config.get_celery_config_dict()
+app = Celery()
+app.conf.update(**celery_config)
+
+
+class Repo:
+    pass
+
+
+repo = Repo()
+repo.plan_repo = None
+repo.task_repo = None
+
+
+@celeryd_after_setup.connect()
+def init(sender, instance, **kwargs):
+    logger.info("Initializing {} worker".format(__name__))
+    repo.plan_repo = PlanRepo(redis_inst)
+    repo.task_repo = TaskRepo(redis_inst)
+
+
+@app.task()
+def store_plan(plan):
+    plan_id = repo.plan_repo.save_new_plan(plan)
+    repo.task_repo.save_new_tasks(plan_id, plan.get_tasks())
+    repo.task_repo.save_dependencies(plan_id, plan.get_dependencies())
+    return plan_id
