470d548b700cb64b502bc917e0608b54a213b12f
kafforch <kafforch@github.com>
2015-12-31 01:07:03 +1100

    Added support for plan level start_on

--- workers/plan_starter.py
+++ workers/plan_starter.py
@@ -1,6 +1,7 @@
 from workers.base import app, repo
 from celery.utils.log import get_task_logger
 from celery.schedules import timedelta
+from utils.time_utils import utcnow, to_utc
 
 TASK_NAME = '{}.start_plans'.format(__name__)
 
@@ -22,7 +23,9 @@ def start_plans():
     for plan_id in repo.plan_repo.get_all_plan_ids():
         plan = repo.plan_repo.get_plan_by_id(plan_id)
         if plan.is_plan_initial():
-            with repo.lock_manager:
-                logger.info("Starting plan {}".format(plan_id))
-                plan.set_plan_as_running()
-                repo.plan_repo.save_plan(plan_id, plan)
+            if plan.get_start_on() is None \
+                    or to_utc(plan.get_start_on()) <= utcnow():
+                with repo.lock_manager:
+                    logger.info("Starting plan {}".format(plan_id))
+                    plan.set_plan_as_running()
+                    repo.plan_repo.save_plan(plan_id, plan)
