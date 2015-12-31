66cc77b1ef3315c67735735f2cc2b46ab680ab74
kafforch <kafforch@github.com>
2015-12-31 12:14:11 +1100

    Separated functional test configuration

--- functional_tests/plan_submitter_tests.py
+++ functional_tests/plan_submitter_tests.py
@@ -67,12 +67,7 @@ class PlanSubmitterTests(BaseFunctionalTestCase):
 
         plan_id = result.get(5)
 
-        self.assertEqual(plan_id, 'c4ca4238a0b923820dcc509a6f75849b')
-
         plan = self.plan_repo.get_plan_by_id(plan_id)
-
-        self.assertIsNotNone(plan)
-        self.assertEqual(len(plan.get_tasks()), 3)
 
         time.sleep(11)
 
