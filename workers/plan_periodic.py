a742181273ce004c6fb86a38afee9460394f9fe9
kafforch <kafforch@github.com>
2016-01-04 00:06:12 +1100

    Added plan submitted_on and completed_on attributes

--- workers/plan_periodic.py
+++ workers/plan_periodic.py
@@ -3,6 +3,7 @@ from celery.utils.log import get_task_logger
 from celery.schedules import timedelta
 from utils.time_utils import is_time_in_the_past
 from model.task_starter_logic import all_tasks_complete
+from utils.time_utils import utcnow_str
 
 START_PLANS_TASK_NAME = '{}.start_plans'.format(__name__)
 COMPLETE_PLANS_TASK_NAME = '{}.complete_plans'.format(__name__)
@@ -33,6 +34,7 @@ def start_plans():
             if plan.get_start_on() is None \
                     or is_time_in_the_past(plan.get_start_on()):
                     logger.info("Starting plan {}".format(plan_id))
+                    plan.set_started_on(utcnow_str())
                     plan.set_plan_as_running()
                     repo.plan_repo.save_plan(plan_id, plan)
 
@@ -46,5 +48,6 @@ def complete_plans():
         if plan.is_plan_running():
             if all_tasks_complete(tasks):
                     logger.info("Completing plan {}".format(plan_id))
+                    plan.set_completed_on(utcnow_str())
                     plan.set_plan_as_complete()
                     repo.plan_repo.save_plan(plan_id, plan)
